25168e690a3dcd80f77beb3978c16759ab7db491
Chris Martinez <smartinez79@protonmail.com>
2019-07-15 06:16:26 -0500

    update to server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
+var handlebars = require("express-handlebars")
 var mongoose = require("mongoose");
+
 var PORT = process.env.PORT || 3000;
 
 // Initialize Express
@@ -11,6 +13,11 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controller/controller.js");
+app.use(routes);
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
